20939c9781dc04a97f44d083c98a4d043e45f6b6
adewale adegoroye <adeguk@gmail.com>
2016-01-02 09:41:00 +0000

    making updates

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,69 @@
+/************************** Nav Slider ***************************************/
+
+        jQuery(document).ready(function ($) {
+
+            var options = {
+                $FillMode: 2,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
+                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $Idle: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $PauseOnHover: 1,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
+
+                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+                $SlideEasing: $JssorEasing$.$EaseOutQuint,          //[Optional] Specifies easing for right to left animation, default value is $JssorEasing$.$EaseOutQuad
+                $SlideDuration: 800,                               //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+                $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+                //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+                //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+                $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
+                $Cols: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+                $ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+                $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+                $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
+                $DragOrientation: 1,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $Cols is greater than 1, or parking position is not 0)
+
+                $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+                    $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
+                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                    $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                    $Rows: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                    $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                    $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                    $Orientation: 1,                                //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+                    $Scale: false                                   //Scales bullets navigator or not while slider scale
+                },
+
+                $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+                    $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
+                    $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                    $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+                }
+            };
+
+            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var bodyWidth = document.body.clientWidth;
+                if (bodyWidth)
+                    jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
+
+
+
 jQuery(document).ready(function($){
-	
+
 	function goToByScroll(id){
 	      // Remove "link" from the ID
 	    id = id.replace("link", "");
@@ -8,21 +72,21 @@ jQuery(document).ready(function($){
 	        scrollTop: $("#"+id).offset().top},
 	        'slow');
 	}
-	
+
 	$('.nav > li > a').click(function(){
-		
+
 		goToByScroll($(this).attr('class'));
-				
+
 	});
-		
-});	
+
+});
 
 
 
 /* -------------------- Check Browser --------------------- */
 
 function browser() {
-	
+
 	//var isOpera = !!(window.opera && window.opera.version);  // Opera 8.0+
 	//var isFirefox = testCSS('MozBoxSizing');                 // FF 0.8+
 	var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
@@ -33,27 +97,27 @@ function browser() {
 	function testCSS(prop) {
 	    return prop in document.documentElement.style;
 	}
-	
+
 	if (isSafari || isChrome) {
-		
+
 		return true;
-		
+
 	} else {
-		
+
 		return false;
-		
+
 	}
-	
+
 }
 
 /* -------------------- Charts --------------------- */
 
 jQuery(document).ready(function($){
-		
+
 	function randNum(){
 		return (Math.floor( Math.random()* (1+40-20) ) ) + 20;
 	}
-	
+
 	if($("#stockPrice").length)
 	{
 		var price = [[1, randNum()-10], [2, randNum()-10], [3, randNum()-10], [4, randNum()],[5, randNum()],[6, 4+randNum()],[7, 5+randNum()],[8, 6+randNum()],[9, 6+randNum()],[10, 8+randNum()],[11, 9+randNum()],[12, 10+randNum()],[13,11+randNum()],[14, 12+randNum()],[15, 13+randNum()],[16, 14+randNum()],[17, 15+randNum()],[18, 15+randNum()],[19, 16+randNum()],[20, 17+randNum()],[21, 18+randNum()],[22, 19+randNum()],[23, 20+randNum()],[24, 21+randNum()],[25, 14+randNum()],[26, 24+randNum()],[27,25+randNum()],[28, 26+randNum()],[29, 27+randNum()], [30, 31+randNum()]];
@@ -68,8 +132,8 @@ jQuery(document).ready(function($){
 					   points: { show: true },
 					   shadowSize: 1
 				   },
-				   grid: { hoverable: true, 
-						   clickable: true, 
+				   grid: { hoverable: true,
+						   clickable: true,
 						   tickColor: "#eee",
 						   borderWidth: 0,
 						 },
@@ -112,9 +176,9 @@ jQuery(document).ready(function($){
 					$("#tooltip").remove();
 					previousPoint = null;
 				}
-		
+
 		});
-		
+
 	}
 
 });
@@ -122,9 +186,9 @@ jQuery(document).ready(function($){
 /* -------------------- Search --------------------- */
 
 jQuery(document).ready(function($){
-	
+
 	$('.search > :input').on('keyup',function(){
-		
+
 		$(this).attr('class', 'activeSearch');
 
 		var count;
@@ -159,31 +223,31 @@ jQuery(document).ready(function($){
 			$(this).wrap('<div class="btn-overlay" />');
 
 			var inner = $(this).html();
-			
+
 			if(browser()) {
 
 				$(this).html('<span>' + inner + '</span>');
-				
-			}	
+
+			}
 
 		}
 
 	});
-	
-		
+
+
 	$('a').each(function(){
 
 		if($(this).hasClass('btnOverlay')) {
-			
+
 			$(this).wrap('<div class="btn-overlay" />');
 
 			var inner = $(this).html();
-			
+
 			if(browser()) {
 
 				$(this).html('<span>' + inner + '</span>');
-				
-			}	
+
+			}
 
 		}
 
@@ -194,15 +258,15 @@ jQuery(document).ready(function($){
 /* -------------------- Twitter --------------------- */
 
 jQuery(document).ready(function($){
-	
+
 	$.getJSON('http://api.twitter.com/1/statuses/user_timeline/lukaszholeczek.json?count=3&callback=?', function(tweets){
 		$("#twitter").html(tz_format_twitter(tweets));
-	}); 
+	});
 
 });
 
 jQuery(document).ready(function($){
-	
+
 	/* ------------------- Fancybox --------------------- */
 
 	(function() {
@@ -234,8 +298,8 @@ jQuery(document).ready(function($){
 
 
 	})();
-	
-	
+
+
 	/* ------------------- Client Carousel --------------------- */
 
 	$('.clients-carousel').flexslider({
@@ -255,12 +319,12 @@ jQuery(document).ready(function($){
 	/* ------------------ Back To Top ------------------- */
 
 	jQuery('#under-footer-back-to-top a').click(function(){
-		jQuery('html, body').animate({scrollTop:0}, 300); 
-		return false; 
+		jQuery('html, body').animate({scrollTop:0}, 300);
+		return false;
 	});
-	
 
-	/* --------------------- Tabs ------------------------ */	
+
+	/* --------------------- Tabs ------------------------ */
 
 		(function() {
 
@@ -290,8 +354,8 @@ jQuery(document).ready(function($){
 			});
 
 		})();
-		
-			
+
+
 });
 
 /* ------------------ Tooltips ----------------- */
@@ -304,10 +368,10 @@ jQuery(document).ready(function() {
 
 });
 
-/* ------------------ Progress Bar ------------------- */	
+/* ------------------ Progress Bar ------------------- */
 
 jQuery(document).ready(function($){
-	
+
 	$(".meter > span").each(function() {
 		$(this)
 		.data("origWidth", $(this).width())
@@ -321,21 +385,21 @@ jQuery(document).ready(function($){
 /* ------------------ Image Overlay ----------------- */
 
 jQuery(document).ready(function () {
-	
+
 	$('.picture').hover(function () {
 		$(this).find('.image-overlay-zoom, .image-overlay-link').stop().fadeTo(150, 1);
 	},function () {
 		$(this).find('.image-overlay-zoom, .image-overlay-link').stop().fadeTo(150, 0);
 	});
-	
+
 });
 
 /* -------------------- Isotope --------------------- */
 
 jQuery(document).ready(function () {
-	
+
 	$('#portfolio-wrapper').imagesLoaded(function() {
-		
+
 		var $container = $('#portfolio-wrapper');
 			$select = $('#filters select');
 
@@ -349,7 +413,7 @@ jQuery(document).ready(function () {
 
 		// update columnWidth on window resize
 		$(window).smartresize(function(){
-		
+
 			$container.isotope({
 			// update columnWidth to a percentage of container width
 			masonry: { columnWidth: $container.width() / 12 }
@@ -362,20 +426,20 @@ jQuery(document).ready(function () {
 		});
 
 		$select.change(function() {
-			
+
 			var filters = $(this).val();
 
 				$container.isotope({
 					filter: filters
 				});
-			
+
 			});
 
 			var $optionSets = $('#filters .option-set'),
 		  	$optionLinks = $optionSets.find('a');
 
 		  	$optionLinks.click(function(){
-			
+
 				var $this = $(this);
 				// don't proceed if already selected
 				if ( $this.hasClass('selected') ) {
@@ -401,9 +465,9 @@ jQuery(document).ready(function () {
 			}
 
 			return false;
-			
+
 		  });
-		
+
 	});
-	
+
 });
